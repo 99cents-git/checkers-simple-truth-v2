78161875dcdd2f953731d90da8a4f36f8434b353
Absolom Mugwagwa <absolom@Absolom-Mugwagwa.local>
2019-01-24 11:14:32 +0200

    finishing slide with product data

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -10,18 +10,19 @@
           <youtube :videoId="currentVideoId" player-width="100%" :player-height="videoHeight"></youtube>
         </div>
         <div class="sidebar-image spinner" v-if="!videoVisible">
-          <div class="sidebar-image-wrapper" v-bind:style="{'background-image': 'url(./static/'+currentSidebarItem.imagePath+')'}"></div>
+           <div class="sidebar-image-wrapper" v-bind:style="{'background-image': 'url(./static/products-bg.png)'}"><img class="product-shot" v-bind:src="'./static/'+currentSidebarItem.imagePath+''"></div>
+          
         </div>
         <div class="sidebar-details p-4 p-lg-5 d-flex flex-column">
           <div class="product mb-4" v-html="currentSidebarItem.productName"></div>
           
-          <div class="d-flex flex-row"><div class="p-2 bg-info" v-for="tag in currentSidebarItem.tags">{{ tag }}</div></div>
-          <div class="details mb-3" v-html="currentSidebarItem.description"></div>
+          <div class="d-flex flex-row"><div class="p-2 bg-info tags tag-justice" v-for="tag in currentSidebarItem.tags"><img src="@/assets/tags-tick.svg" alt="">  {{ tag }}</div></div>
+          <div class="details product-desc p-2 p-lg-4" v-html="currentSidebarItem.description"></div>
           <div class="sidebar-details-entry">
             <div class="header p-4 d-flex flex-column">
               NUTRITIONAL INFO
             </div>
-            <div class="contents" v-for="nutritionalInfo in currentSidebarItem.nutritionalInfo"><ul><li>{{ nutritionalInfo }}</li></ul></div>
+            <div class="contents" v-for="nutritionalInfo in currentSidebarItem.nutritionalInfo"><ul><li class="nutritional-info"><img src="@/assets/tags-tick.svg" alt="">{{ nutritionalInfo }}</li></ul></div>
           </div>
          
         
@@ -38,6 +39,72 @@
 </script>
 <style scoped lang="less">
   @import './SidebarStyles';
+
+.tag-justice {
+  justify-content: space-evenly;
+}
+.tags {
+  
+  background-color: #F0ECE6 !important;
+    height: 3em;
+    width: 40%;
+    border-radius: 35px;
+    color:#333333;
+    text-transform: uppercase;
+    margin: 0.5rem;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 0 30px;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    font-size: 0.9rem;
+    font-weight: bold;
+    text-align: center;
+    line-height: 1.4rem;
+  }
+
+
+.tags > img {
+  padding-right: 0.3rem;
+}
+
+.sidebar-text-colour {
+  color: #828282;
+}
+
+.nutritional-info {
+  color: #4F4F4F;
+  font-size: 1rem;
+  text-align: center;
+}
+
+.nutritional-info > img {
+  margin-top: -.1em;
+  padding-right: 0.3rem;
+}
+
+.product-desc {
+  color:#828282;
+  font-size: 1.2rem;
+}
+
+.product-shot {
+max-height: 100%;  
+    max-width: 100%; 
+    width: auto;
+    height: auto;
+    position: absolute;  
+    top: 0;  
+    bottom: 0;  
+    left: 0;  
+    right: 0;  
+    margin: auto;
+}
 
   
 </style>
